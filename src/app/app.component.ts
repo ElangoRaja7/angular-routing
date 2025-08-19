43cb1b4891820457169e1e4fa17542d6bfd57631
ElangoRaja7 <elangorajece2k10@gmail.com>
2025-08-20 01:20:57 +0530

    Adding Link throug memeber variable of a app component class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet,RouterLink } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AccountComponent } from './account/account.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,SettingsComponent,RouterLink],
+  imports: [RouterOutlet,HomeComponent,SettingsComponent,RouterLink,AccountComponent,ProfileComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learn-angular-routing';
+  routes=[{
+    linkName:'account',url:'account'
+  },
+  {
+    linkName:'profile',url:'profile'
+  }];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
 import { ErrorComponent } from './error/error.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AccountComponent } from './account/account.component';
 
 export const routes: Routes = [
     {path:'', redirectTo: '/home', pathMatch:'full'},
     {path:'home', component:HomeComponent},
     {path:'settings', component:SettingsComponent},
+    {path:'account',component:AccountComponent},
+    {path:'profile',component:ProfileComponent},
      {path:'**', component:ErrorComponent}
 ];
