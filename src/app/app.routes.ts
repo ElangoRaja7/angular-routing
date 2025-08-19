c21b51325d800e473314094e4f6bde28445e91f6
ElangoRaja7 <elangorajece2k10@gmail.com>
2025-08-20 00:38:23 +0530

    routing example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/home', pathMatch:'full'},
+    {path:'home', component:HomeComponent},
+    {path:'settings', component:SettingsComponent},
+     {path:'**', component:ErrorComponent}
+];
